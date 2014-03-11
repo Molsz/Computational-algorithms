522805a6a9252010185eea2bc8734554605768ce
Molsz <molszew@onet.pl>
2014-03-11 16:51:28 +0100

    improved algo, +gauss/gamma

--- Root_finding/functions.py
+++ Root_finding/functions.py
@@ -1,12 +1,32 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from __future__ import division
+from scipy import stats
 import math
 
+def normal(u=0, s=1):
+    def fun(x):
+        return stats.distributions.norm.cdf(x, loc=u, scale=s)
+    return fun
+
+def gamma(a, l=1):
+    def fun(x):
+        return stats.distributions.gamma.cdf(x, a, loc=0, scale=l)
+    return fun
+
+def normal_drv(u=0, s=1):
+    def fun(x):
+        return stats.distributions.norm.pdf(x, loc=u, scale=s)
+    return fun
+
+def gamma_drv(a, l=1):
+    def fun(x):
+        return stats.distributions.gamma.pdf(x, a, loc=0, scale=l)
+    return fun
 
 def polynomial(*args):
     """Returns a polynomial function with closed coefficients.
-    Eg. polynomial(3, 2, 1) = 3x^2 + 2x + 1
+    Eg. polynomial(3, 2, 1) = fun(x) returning 3x^2 + 2x + 1
     """
     def fun(x):
         return sum([arg*pow(x, i) for i, arg in enumerate(reversed(args))])
@@ -16,7 +36,7 @@ def func_a(x):
     """e^(-x) - x"""
     return math.exp(-x) - x
 
-def func_a_deriv(x):
+def func_a_drv(x):
     """-e^(-x) - 1"""
     return -math.exp(-x) - 1
 
@@ -26,7 +46,7 @@ def func_b(a):
         return math.exp(-a*x) - x
     return fun
 
-def func_b_deriv(a):
+def func_b_drv(a):
     """-a*e^(-ax) - 1"""
     def fun(x):
         return -a * math.exp(-a*x) - 1
@@ -38,7 +58,7 @@ def func_c(a):
         return a - math.sin(1/x)
     return fun
 
-def func_c_deriv(a):
+def func_c_drv(a):
     """(cos(1/x))/x^2"""
     def fun(x):
         return math.cos(1/x)/x**2
--- Root_finding/main.py
+++ Root_finding/main.py
@@ -1,40 +1,58 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from __future__ import division
 from functions import *
 import math
 
 def samesign(a, b):
     return a*b > 0
 
-def bisect(fun, a, b, eps=1e-6):
+def bisect(func, a, b, val=0, eps=1e-10):
+    """For given function func and interval [a, b] returns c
+    with accuracy >= eps such that f(c) = val"""
     a, b = min(a, b), max(a, b)
+    fun_a = func(a) - val
+    assert not samesign(fun_a, func(b) - val)
+    """Calculating number of iterations from formula"""
     ln = math.log
     num_iters = int(math.ceil((ln(b-a) - ln(eps))/ln(2)))
     for i in xrange(num_iters):
-        c = (a + b)/2
-        if samesign(fun(a), fun(c)):
+        c = (a + b)/2.
+        fun_c = func(c) - val
+        if samesign(fun_a, fun_c):
             a = c
+            fun_a = fun_c
         else:
             b = c
+    return (a + b)/2.
+
+def newton(func, func_drv, c, val=0, eps=1e-10, dt=1e-6, num_iters=100):
+    """For given function func, its drvative func_drv and
+    starting point c returns c with accuracy >= eps such that f(c) = val
+    returns c prematurely if fun(c) stops converging."""
+    fun_c = func(c) - val
+    delta_c = dt + 1
+    for i in xrange(num_iters):
+        if abs(fun_c) > eps or delta_c > dt:
+            delta_c = fun_c/func_drv(c)
+            c = c - delta_c
+            fun_c = func(c) - val
+        else:
+            return c
     return c
 
-def tangent(fun, fun_deriv, x, eps=1e-6):
-    c = x
-    while abs(fun(c)) > eps:
-        c = c - fun(c)/fun_deriv(c)
-    return c
-
-def secant(fun, a, b, eps=1e-6):
+def secant(func, a, b, val=0, eps=1e-6, num_iters=100):
     a, b = min(a, b), max(a, b)
-    c = b - fun(b)*(b - a)/(fun(b) - fun(a))
-    while abs(fun(c)) > eps:
-        a, b = b, c
-        c = b - fun(b)*(b - a)/(fun(b) - fun(a))
+    fun_a, fun_b = func(a), func(b)
+    c = b - fun_b*(b - a)/(fun_b - fun_a)
+    for i in xrange(num_iters):
+        if abs(func(c) - val) > eps:
+            a, b = b, c
+            fun_a, fun_b = func(a), func(b)
+            c = b - fun_b*(b - a)/(fun_b - fun_a)
     return c
 
 if __name__ == '__main__':
     print 'Running all methods on example function e^(-x) - x...'
     print 'Bisection method: %.9f' % bisect(func_a, a=-2, b=4)
-    print 'Newton method: %.9f' % tangent(func_a, func_a_deriv, x=0)
+    print 'Newton method: %.9f' % newton(func_a, func_a_drv, c=0)
     print 'Secant method: %.9f' % secant(func_a, a=-2, b=4)
